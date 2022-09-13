2b37fb5c1c836b85c83e8852dceb9509bad0a8d9
AVNISH KUMAR <47257865+avnishkrsharma3@users.noreply.github.com>
2022-09-14 01:26:15 +0530

    Time: 126 ms (8.29%), Space: 50.9 MB (24.09%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+        
+        for(int i  = 0; i < data.length; i++){
+            
+            int nByte = nByte(data[i]);
+            System.out.println(nByte + "  " );
+            if(nByte == 1 || nByte > 4) return false;
+            int j  = 0;
+            for(j = i+1; j < i+nByte; j++){
+                if(j >= data.length || isCorrect(data[j]) == false)
+                    return false;
+            }
+            i = j-1;          
+        }
+          return true;
+    }
+    
+    public int nByte(int val){
+        int k = 7;
+        int bitMask = 1 << k;
+        while((val & bitMask) >= 1 && k > 0){
+            k--;
+            bitMask = 1 << k;
+        }
+        int ans = 7 - k;
+        return ans;
+    }
+    public boolean isCorrect(int val){
+        int k = 7;
+        int bitMask = 1 << k;
+        int bitMask2 = 1 << 6;
+         if(((val & bitMask) >= 1 ) && ((val & bitMask2) == 0))
+             return true;
+         return false;
+    }
+}
