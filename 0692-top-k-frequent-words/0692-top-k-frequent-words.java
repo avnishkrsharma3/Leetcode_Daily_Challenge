a30557ebf40e2b24ae00f9f3737acf5bd83c2933
AVNISH KUMAR <47257865+avnishkrsharma3@users.noreply.github.com>
2022-10-20 00:18:28 +0530

    Time: 21 ms (6.97%), Space: 45.5 MB (32.96%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> tmap = new TreeMap<>(Collections.reverseOrder());
+        for(String word : words){
+            tmap.put(word, tmap.getOrDefault(word, 0) + 1);
+        }
+        Pair[] arr = new Pair[tmap.size()];
+        int i = 0;
+        for(Map.Entry<String, Integer> iter: tmap.entrySet()){
+            Pair p = new Pair(iter.getValue(), iter.getKey());
+            arr[i++] = p;
+        }
+        Arrays.sort(arr, (Pair p1, Pair p2) -> {
+            if(p1.cnt < p2.cnt){
+                return 1;
+            }
+            else if(p1.cnt == p2.cnt){
+                return (p1.str).compareTo(p2.str);
+            }
+            else 
+                return -1;
+        });
+        List<String> retList = new ArrayList<>();
+        for(int j = 0;  j < k; j++){
+            Pair p = arr[j];
+            retList.add(p.str);
+        }
+        return retList;
+    }
+    class Pair{
+        int cnt;
+        String str;
+        Pair(int cnt, String str){
+            this.cnt = cnt;
+            this.str = str;
+        }
+    }
+}
