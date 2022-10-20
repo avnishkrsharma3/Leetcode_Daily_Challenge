d0f344377f567eb82ad6fe52dcd40266d1d186a2
AVNISH KUMAR <47257865+avnishkrsharma3@users.noreply.github.com>
2022-10-20 23:30:41 +0530

    Time: 8 ms (80.25%), Space: 45.4 MB (65.64%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,51 +1,52 @@
 class Solution {
-    String ans ="";
+    StringBuilder ans ;
     public String intToRoman(int num) {
+        ans = new StringBuilder();
         util(num);
-        return ans;
+        return ans.toString();
     }
     public void util(int num){  // 994
         if(num == 0)
             return;
         if(num >= 1000){
-            ans += "M";
+            ans.append("M");
             num -= 1000;
         }
         else if(num + 100 >= 1000){
-            ans += "CM";
+            ans.append("CM");
             num -= 900;
         }else if(num >= 500){
-            ans += "D";
+            ans.append("D");
             num -= 500;
         }else if(num + 100 >= 500){
-            ans += "CD";
+            ans.append("CD");
             num -= 400;
         }else if(num >= 100){
-            ans += "C";
+            ans.append("C");
             num -= 100;
         }else if(num  + 10 >= 100){
-            ans += "XC";
+            ans.append("XC");
             num -= 90;
-        }else if(num>=50){ // // 80
-            ans += "L";
+        }else if(num>=50){ // // 80           
+            ans.append("L");
             num -= 50;
         }else if(num+10 >= 50){
-            ans += "XL";
+            ans.append("XL");
             num -= 40;
         }else if(num >= 10){
-            ans += "X";
+            ans.append("X");
             num -= 10;
         }else if(num + 1 >= 10){
-            ans += "IX";
+            ans.append("IX");
             num -= 9;
         }else if(num >= 5){
-            ans += "V";
+            ans.append("V");
             num -= 5;
         }else if(num +1 >= 5){
-            ans += "IV";
+            ans.append("IV");
             num -= 4;
         }else{
-            ans += "I";
+            ans.append("I");
             num -= 1;
         }
         util(num);
