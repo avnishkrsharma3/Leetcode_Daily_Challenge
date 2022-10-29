a69838cd252c3e1cc8289f58594ec5b019bdbdd4
AVNISH KUMAR <47257865+avnishkrsharma3@users.noreply.github.com>
2022-10-29 22:51:29 +0530

    Time: 113 ms (67.12%), Space: 125.3 MB (37.90%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+      Pair [] arr = new Pair[plantTime.length];
+        for(int i = 0; i < plantTime.length; i++){
+            Pair p = new Pair(plantTime[i], growTime[i]);
+            arr[i] = p;
+        }
+        Arrays.sort(arr, (p1, p2) -> p2.gt - p1.gt);
+        
+        int time = 0;
+        int grwt = 0;
+        for(int i = 0; i < arr.length; i++){
+            time += arr[i].pt;
+            grwt += arr[i].gt;
+            if(i < arr.length - 1){
+                if( grwt >= arr[i+1].pt ){
+                    grwt -= arr[i+1].pt;
+                    if(grwt >= arr[i+1].gt ){
+                        grwt -= arr[i+1].gt;
+                    }
+                    else{
+                        grwt = 0;
+                    }
+                }
+                else{
+                    grwt = 0;
+                }
+            }
+        }
+        
+        return time +  grwt ;
+    }
+    class Pair{
+        int pt = 0;
+        int gt = 0;
+        Pair(int p, int g){
+            pt = p;
+            gt = g;
+        }
+    }
+}
